991ac113cc87ef8734dcb92aaaa553e331eb9140
aelasefa <aymanelasefar1337@gmail.com>
2025-12-02 19:42:42 +0100

    start working in ex03

---
+++ cpp01/ex03/Weapon.hpp
@@ -0,0 +1,14 @@
+#ifndef WEAPON_HPP
+#define WEAPON_HPP
+
+#include <string>
+#include <iostream>
+
+class Weapon {
+private:
+	std::string type;	
+public:
+	const std::string&  getType()const;
+	void setType(std::string value);
+};
+#endif
