0c2d8cb8136b1b1289b2717587bba815210a2110
aelasefa <aymanelasefar1337@gmail.com>
2025-12-04 21:24:33 +0100

    ex06 is finished

---
+++ cpp01/ex06/main.cpp
@@ -0,0 +1,42 @@
+#include "Harl.hpp"
+
+int main(int ac, char **av)
+{
+	Harl harl;
+	if (ac != 2)
+	{
+		std::cout << "Usage: ./harlFilter <level>" << std::endl;
+        return 1;
+	}
+	std::string level = av[1];
+    int lvl = -1;
+    if (level == "DEBUG") lvl = 0;
+    else if (level == "INFO") lvl = 1;
+    else if (level == "WARNING") lvl = 2;
+    else if (level == "ERROR") lvl = 3;
+
+    switch (lvl)
+    {
+        case 0:
+            std::cout << "[ DEBUG ]" << std::endl;
+            harl.complain("DEBUG");
+            std::cout << std::endl;
+        case 1:
+            std::cout << "[ INFO ]" << std::endl;
+            harl.complain("INFO");
+            std::cout << std::endl;
+        case 2:
+            std::cout << "[ WARNING ]" << std::endl;
+            harl.complain("WARNING");
+            std::cout << std::endl;
+        case 3:
+            std::cout << "[ ERROR ]" << std::endl;
+            harl.complain("ERROR");
+            std::cout << std::endl;
+            break;
+        default:
+            std::cout << "[ Probably complaining about insignificant problems ]" 
+                      << std::endl;
+    }
+    return 0;
+}
