620e55a9787ba1dc5f3615e27ccc92c0f2b7b5bd
aelasefa <aymanelasefar1337@gmail.com>
2026-01-07 13:15:37 +0100

    chnage constructor from public to private

--- cpp06/ex01/Serializer.hpp
+++ cpp06/ex01/Serializer.hpp
@@ -5,13 +5,17 @@
 typedef unsigned long uintptr_t;
 class Serializer
 {
-public:
+private:
 	Serializer();
-	Serializer(const Serializer &other);
-	~Serializer();
+
+public:
 	static uintptr_t serialize(Data* ptr);
 	static Data* deserialize(uintptr_t raw);
+
+	Serializer(const Serializer &other);
+	~Serializer();
 	Serializer &operator=(const Serializer &other);
+
 };
 
 #endif
